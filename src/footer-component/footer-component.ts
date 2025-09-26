b71da7e1b48e34f23cebc76ff9a0b1e04cbf1ee3
carloselcrackt1 <carlosalejandroauquimolina@gmail.com>
2025-09-25 21:53:39 -0500

    Segundo Committ

--- src/footer-component/footer-component.ts
+++ src/footer-component/footer-component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-footer-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer-component.html',
   styleUrl: './footer-component.css'
 })
 export class FooterComponent {
+  elements: Footer[]=[
+    {id:1,titulo:"Primer cuadro",parrafo:"Este es el primer cuadro que se crea de manera iteraticva"},
+    {id:2, titulo:"Segundo Cuadro",parrafo:"Segundo cuadro de manera interactiva"}
+  ]
+}
 
+export interface Footer{
+  id: number;
+  titulo: String;
+  parrafo: String;
 }
