6d3642ff0dd42a9acab95c93a00a0c9980c84137
carloselcrackt1 <carlosalejandroauquimolina@gmail.com>
2025-09-25 22:19:11 -0500

    Ultimo commit

--- src/navbar/navbar.ts
+++ src/navbar/navbar.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
-
+  paths: NavbarC[] =[
+    {name: "Inicio", pathi: "/home"},
+    {name: "Medio", pathi: "/medio"},
+    {name: "Final", pathi: "/fin"}
+  ]
 }
+export interface NavbarC{
+  name:String;
+  pathi:String;
+}
+
